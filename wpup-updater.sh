e62930dedd74e70e6e2ab624b949800f1a396a8b
koyu <me@koyu.space>
2021-07-08 01:32:48 +0200

    Randomize updater

--- wpup-updater.sh
+++ wpup-updater.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sleep 2
 sudo rm /usr/bin/wpup
-sudo wget -q -o /dev/null -O /usr/bin/wpup https://raw.githubusercontent.com/koyuspace/wpup/main/wpup.py
+sudo wget -q -o /dev/null -O /usr/bin/wpup https://raw.githubusercontent.com/koyuspace/wpup/main/wpup.py?$RANDOM
 sudo chmod +x /usr/bin/wpup
